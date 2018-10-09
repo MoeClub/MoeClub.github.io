176f50c9e62816679703a9add6a2144702c404e2
MoeClub <32381827+MoeClub@users.noreply.github.com>
2018-10-09 13:48:18 +0800

    Update install.sh

--- ss/simple/install.sh
+++ ss/simple/install.sh
@@ -31,7 +31,7 @@ while [[ $# -ge 1 ]]; do
 arch="$(dpkg --print-architecture)"
 [ -n "$arch" ] || exit 1
 [ "$arch" == 'amd64' -o "$arch" == 'i386' ] || exit 1
-wget --no-check-certificate -qO "/tmp/shadowsocks-libev_2.4.6-1_${arch}.deb" "https://moeclub.org/attachment/LinuxShell/libev/shadowsocks-libev_2.4.6-1_${arch}.deb"
+wget --no-check-certificate -qO "/tmp/shadowsocks-libev_2.4.6-1_${arch}.deb" "https://moeclub.github.io/ss/simple/shadowsocks-libev_2.4.6-1_${arch}.deb"
 dpkg -i "/tmp/shadowsocks-libev_2.4.6-1_${arch}.deb"
 [ -f /etc/shadowsocks-libev/config.json ] && {
 [ -z $Port ] && Port='8080'
